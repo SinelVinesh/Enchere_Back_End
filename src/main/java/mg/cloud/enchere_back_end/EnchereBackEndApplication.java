1296e66ecd3c9f665f6395652e6f2a39a0fdaed8
SoleilDivin <vinesh.sinel@gmail.com>
2023-01-13 11:08:23 +0300

    Initializing the project

---
+++ src/main/java/mg/cloud/enchere_back_end/EnchereBackEndApplication.java
@@ -0,0 +1,13 @@
+package mg.cloud.enchere_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnchereBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnchereBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mg/cloud/enchere_back_end/EnchereBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.cloud.enchere_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnchereBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
