67e2af4c00652f7f0d4443bf8cd0eaece6debfe8
SoleilDivin <vinesh.sinel@gmail.com>
2023-01-20 19:12:42 +0300

    Add cord mappings

--- src/main/java/mg/cloud/enchere_back_end/EnchereBackEndApplication.java
+++ src/main/java/mg/cloud/enchere_back_end/EnchereBackEndApplication.java
@@ -2,6 +2,10 @@ package mg.cloud.enchere_back_end;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EnchereBackEndApplication {
@@ -10,4 +14,13 @@ public class EnchereBackEndApplication {
         SpringApplication.run(EnchereBackEndApplication.class, args);
     }
 
+    @Bean
+    WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/*").allowedOriginPatterns("*");
+            }
+        };
+    }
 }
